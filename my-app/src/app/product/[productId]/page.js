a6682a323f0803f90281faaf787fb6bd1d81d5d8
Ranajit Das <dasranajit410@gmail.com>
2024-10-09 13:46:25 +0530

    Add the dynamic route

---
+++ my-app/src/app/product/[productId]/page.js
@@ -0,0 +1,7 @@
+export default function ProductList({params}) {
+    return (
+        <>
+            <h2>Product list {params.productId}</h2>
+        </>
+    );
+}
---
+++ my-app/src/app/product/page.js
@@ -0,0 +1,10 @@
+export default function ProductList({params}) {
+    return (
+        <>
+            <h2>Product list {params.productId}</h2>
+            <h1>Product 1</h1>
+            <h1>Product 2</h1>
+            <h1>Product 3</h1>
+        </>
+    );
+}
